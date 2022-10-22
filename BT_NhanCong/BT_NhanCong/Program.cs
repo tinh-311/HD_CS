26c9372a5a9e9cedebc9f5158449bea84cb06ce4
Timmo <timmo.n@itcgroup.io>
2022-10-22 23:05:02 +0700

    init

---
+++ BT_NhanCong/BT_NhanCong/Program.cs
@@ -0,0 +1,20 @@
+﻿using BT_NhanCong.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_NhanCong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CNhancong cNhancong = new CNhancong("nv01", "Timmo Nguyen", LoaiNhanCong.CoTayNgheCao, 15);
+            System.Console.WriteLine(cNhancong.tiencong());
+
+            Console.ReadKey();
+        }
+    }
+}
